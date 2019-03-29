4d659177a8d0d11d33e2d0f834d4bed2cf76f919
Hargobind Atwal <ethaipath@gmail.com>
2019-03-29 10:18:45 -0700

    getting cl input

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
-
+#include <stdlib.h>
 /**
  * Main
  */
@@ -8,12 +8,20 @@ int main(int argc, char **argv)
 {
   // Parse command line
 
+  printf("There are %d command line argument(s):\n", argc);
+
+  char *dirpath = malloc(128 * sizeof(char));
+
+  dirpath = argv[1];
+
+  printf("dirpath \"%s\"\n", dirpath);
 
   // Open directory
+  DIR *d = opendir(".");
 
   // Repeatly read and print entries
 
   // Close directory
-
+  closedir(d);
   return 0;
 }
