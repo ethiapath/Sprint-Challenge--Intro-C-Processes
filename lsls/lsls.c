8e896bb680057b40d26428c5e43c79623cf5c4d5
Hargobind Atwal <ethaipath@gmail.com>
2019-03-29 10:27:21 -0700

    prints out files

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 /**
  * Main
  */
@@ -12,14 +13,31 @@ int main(int argc, char **argv)
 
   char *dirpath = malloc(128 * sizeof(char));
 
-  dirpath = argv[1];
+  if (argv[1] == NULL) {
+    dirpath = ".";
+  } else {
+    dirpath = argv[1];
+
+  }
 
   printf("dirpath \"%s\"\n", dirpath);
 
   // Open directory
   DIR *d = opendir(".");
+  if (!d) {
+    printf("ERROR: dir is %s\n", *d);
+    return 1;
+  }
 
   // Repeatly read and print entries
+
+  struct dirent * dirReader = readdir(d);
+   while (dirReader != NULL)
+   {
+    dirReader = readdir(d);
+    printf(" \"%s\" \n", dirReader->d_name);
+
+  } 
 
   // Close directory
   closedir(d);
